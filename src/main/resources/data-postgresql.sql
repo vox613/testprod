a49e3fedecab8a9b98b5d5225ab30bdeb77bb563
vox613 <vox613@gmail.com>
2019-12-19 03:37:43 +0300

    3

--- src/main/resources/data-postgresql.sql
+++ src/main/resources/data-postgresql.sql
@@ -20,23 +20,12 @@ VALUES (nextval('category_id_seq'), 'Одежда'),
        (nextval('category_id_seq'), 'Спорттовары'),
        (nextval('category_id_seq'), 'Услуги');
 
-INSERT INTO users (id, username, first_name, last_name, password,
-                   phone_number, email, city_id, role_id)
-VALUES (nextval('users_id_seq'), 'username1', 'first name 1', 'last name 1',
-        '$2a$10$UqSbP/Wsulh3c/t058fW8ei04H1U1yThkiJ311mJANkt5Oi0ZqMK6', '1234565', 'email1',
-        (SELECT id FROM city WHERE id = 1), 1),
-       (nextval('users_id_seq'), 'username2', 'first name 2', 'last name 2',
-        '$2a$10$XcLXPHgdnDr2m5kC7yFfYOij3bYw3MB6Sd0WVI9Qk9o0htVJr3kOm', '1234564', 'email2',
-        (SELECT id FROM city WHERE id = 2), 2),
-       (nextval('users_id_seq'), 'username3', 'first name 3', 'last name 3',
-        '$2a$10$XcLXPHgdnDr2m5kC7yFfYOij3bYw3MB6Sd0WVI9Qk9o0htVJr3kOm', '1234563', 'email3',
-        (SELECT id FROM city WHERE id = 3), 2),
-       (nextval('users_id_seq'), 'username4', 'first name 4', 'last name 4',
-        '$2a$10$XcLXPHgdnDr2m5kC7yFfYOij3bYw3MB6Sd0WVI9Qk9o0htVJr3kOm', '1234562', 'email4',
-        (SELECT id FROM city WHERE id = 4), 2),
-       (nextval('users_id_seq'), 'username5', 'first name 5', 'last name 5',
-        '$2a$10$XcLXPHgdnDr2m5kC7yFfYOij3bYw3MB6Sd0WVI9Qk9o0htVJr3kOm', '1234561', 'email5',
-        (SELECT id FROM city WHERE id = 1), 2);
+INSERT INTO users (id, username, first_name, last_name, password, phone_number, email, city_id, role_id)
+VALUES (nextval('users_id_seq'), 'username1', 'first name 1', 'last name 1', '$2a$10$UqSbP/Wsulh3c/t058fW8ei04H1U1yThkiJ311mJANkt5Oi0ZqMK6', '1234565', 'email1', (SELECT id FROM city WHERE id = 1), 1),
+       (nextval('users_id_seq'), 'username2', 'first name 2', 'last name 2', '$2a$10$XcLXPHgdnDr2m5kC7yFfYOij3bYw3MB6Sd0WVI9Qk9o0htVJr3kOm', '1234564', 'email2', (SELECT id FROM city WHERE id = 2), 2),
+       (nextval('users_id_seq'), 'username3', 'first name 3', 'last name 3', '$2a$10$XcLXPHgdnDr2m5kC7yFfYOij3bYw3MB6Sd0WVI9Qk9o0htVJr3kOm', '1234563', 'email3', (SELECT id FROM city WHERE id = 3), 2),
+       (nextval('users_id_seq'), 'username4', 'first name 4', 'last name 4', '$2a$10$XcLXPHgdnDr2m5kC7yFfYOij3bYw3MB6Sd0WVI9Qk9o0htVJr3kOm', '1234562', 'email4', (SELECT id FROM city WHERE id = 4), 2),
+       (nextval('users_id_seq'), 'username5', 'first name 5', 'last name 5', '$2a$10$XcLXPHgdnDr2m5kC7yFfYOij3bYw3MB6Sd0WVI9Qk9o0htVJr3kOm', '1234561', 'email5', (SELECT id FROM city WHERE id = 1), 2);
 
 
 INSERT INTO message (id, body, sender_id, receiver_id)
@@ -49,19 +38,20 @@ VALUES (nextval('message_id_seq'), 'Hello. I want to get my purchase.',
        (nextval('message_id_seq'), 'Ok, I will call you tomorrow',
         (SELECT id FROM users WHERE username = 'username2'), (SELECT id FROM users WHERE username = 'username3'));
 
-INSERT INTO lot (id, category_id, condition_id, creation_time, last_mod_time, description,
-                  user_id, current_price, min_price, max_price, step_price, city_id, name)
-VALUES (nextval('lots_id_seq'), 1, 1, now(), now(), 'Simple car', (SELECT id FROM users WHERE username = 'username3'),
-        1001, 500, 1500, 75, 1, 'Car 1'),
-       (nextval('lots_id_seq'), 1, 3, now(), now(), 'Simple car', (SELECT id FROM users WHERE username = 'username3'),
-        1002, 500, 1500, 75, 2, 'Car 2'),
-       (nextval('lots_id_seq'), 1, 3, now(), now(), 'Simple phone', (SELECT id FROM users WHERE username = 'username2'),
-        1003, 500, 1500, 75, 3, 'Phone 1'),
-       (nextval('lots_id_seq'), 1, 3, now(), now(), 'Simple phone', (SELECT id FROM users WHERE username = 'username2'),
-        1004, 500, 1500, 75, 4, 'Phone 2'),
-       (nextval('lots_id_seq'), 1, 3, now(), now(), 'Simple car', (SELECT id FROM users WHERE username = 'username4'),
-        1005, 500, 1500, 75, 3, 'Car 3'),
-       (nextval('lots_id_seq'), 2, 2, now(), now(), 'Simple car', (SELECT id FROM users WHERE username = 'username4'),
-        1006, 500, 1500, 75, 2, 'Car 4'),
-       (nextval('lots_id_seq'), 2, 2, now(), now(), 'Simple car', (SELECT id FROM users WHERE username = 'username4'),
-        1007, 500, 1500, 75, 1, 'Car 5');
+INSERT INTO lot (id, category_id, condition_id, creation_time, last_mod_time, description, user_id, current_price, min_price, max_price, step_price, city_id, name)
+VALUES (nextval('lots_id_seq'), 1, 1, now(), now(), 'Simple car', (SELECT id FROM users WHERE username = 'username3'), 1001, 500, 1500, 75, 1, 'Car 1'),
+       (nextval('lots_id_seq'), 1, 3, now(), now(), 'Simple car', (SELECT id FROM users WHERE username = 'username3'), 1002, 500, 1500, 75, 2, 'Car 2'),
+       (nextval('lots_id_seq'), 1, 3, now(), now(), 'Simple phone', (SELECT id FROM users WHERE username = 'username2'), 1003, 500, 1500, 75, 3, 'Phone 1'),
+       (nextval('lots_id_seq'), 1, 3, now(), now(), 'Simple phone', (SELECT id FROM users WHERE username = 'username2'), 1004, 500, 1500, 75, 4, 'Phone 2'),
+       (nextval('lots_id_seq'), 1, 3, now(), now(), 'Simple car', (SELECT id FROM users WHERE username = 'username4'), 1005, 500, 1500, 75, 3, 'Car 3'),
+       (nextval('lots_id_seq'), 2, 2, now(), now(), 'Simple car', (SELECT id FROM users WHERE username = 'username4'), 1006, 500, 1500, 75, 2, 'Car 4'),
+       (nextval('lots_id_seq'), 2, 2, now(), now(), 'Simple car', (SELECT id FROM users WHERE username = 'username4'), 1007, 500, 1500, 75, 1, 'Car 5');
+
+-- INSERT INTO photo (id, deleted, url, lot_id)
+-- VALUES  (nextval('photo_id_seq'), false, 'https://drive.google.com/uc?id=1XcUvWoJyIqnm4AFCDvjktE_BskUobAb-&export=download', 9),
+--         (nextval('photo_id_seq'), false, 'https://drive.google.com/uc?id=1XcUvWoJyIqnm4AFCDvjktE_BskUobAb-&export=download', 9),
+--         (nextval('photo_id_seq'), false, 'https://drive.google.com/uc?id=1XcUvWoJyIqnm4AFCDvjktE_BskUobAb-&export=download', 9),
+--         (nextval('photo_id_seq'), false, 'https://drive.google.com/uc?id=1XcUvWoJyIqnm4AFCDvjktE_BskUobAb-&export=download', 9),
+--         (nextval('photo_id_seq'), false, 'https://drive.google.com/uc?id=1XcUvWoJyIqnm4AFCDvjktE_BskUobAb-&export=download', 9),
+--         (nextval('photo_id_seq'), false, 'https://drive.google.com/uc?id=1XcUvWoJyIqnm4AFCDvjktE_BskUobAb-&export=download', 9),
+--         (nextval('photo_id_seq'), false, 'https://drive.google.com/uc?id=1XcUvWoJyIqnm4AFCDvjktE_BskUobAb-&export=download', 9),
